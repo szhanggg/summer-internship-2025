4f162ea641d10e19ce543928986951dd29e1a407
szhanggg <stevez9101@gmail.com>
2025-07-17 13:22:07 -0400

    ok

--- unet/model.py
+++ unet/model.py
@@ -8,7 +8,7 @@ import sys
 import pytorch_lightning as pl
 
 class UNET(nn.Module):
-    def __init__(self, in_channels=16, classes=1):
+    def __init__(self, in_channels=16, decoder_classes=64, num_classes = 1, head_dropout = 0.2, output_shape=(91, 40)):
         super().__init__()
         self.layers = [in_channels, 64, 128, 256, 512, 1024]
         
@@ -24,7 +24,16 @@ class UNET(nn.Module):
         
         self.max_pool_2x2 = nn.MaxPool2d(kernel_size=2, stride=2)
         
-        self.final_conv = nn.Conv2d(64, classes, kernel_size=1)
+        self.final_conv = nn.Conv2d(64, decoder_classes, kernel_size=1)
+
+        self.prediction_head = nn.Sequential(
+            nn.Conv2d(decoder_classes, num_classes,
+                      kernel_size=3, stride=1, padding=1),
+            nn.Dropout(head_dropout),
+            nn.Upsample(size=output_shape,
+                        mode='bilinear',
+                        align_corners=False)
+        )
 
         
     def __double_conv(self, in_channels, out_channels):
@@ -59,9 +68,8 @@ class UNET(nn.Module):
             x = double_conv_up(concatenated)
             
         x = self.final_conv(x)
-        x = F.interpolate(x, size=(91, 40), mode='bilinear', align_corners=False)
 
-        x = x.squeeze(1)
+        x = self.prediction_head(x)
         
         return x
 
@@ -71,13 +79,29 @@ class UNET(nn.Module):
         x = (x > 0.5).float()
         return x
 
+class DiceLoss(nn.Module):
+    def __init__(self, smooth=1.0):
+        super().__init__()
+        self.smooth = smooth
+ 
+    def forward(self, logits, targets):
+        probs = torch.sigmoid(logits).view(-1)
+        targets = targets.view(-1)
+        intersection = torch.sum(probs * targets)
+        cardinality = torch.sum(probs + targets)
+        dice_score = (2. * intersection + self.smooth) / (cardinality + self.smooth)
+        return 1. - dice_score
+
 class LightningUNET(pl.LightningModule):
-    def __init__(self, in_channels=3, classes=1):
+    def __init__(self, in_channels=3, classes=1, dice_weight=0.5):
         super().__init__()
         self.in_channels = in_channels
         self.classes = classes
-        self.model = UNET(self.in_channels, self.classes)
+        self.model = UNET(in_channels=self.in_channels, num_classes=self.classes)
         self.iou = JaccardIndex(task="binary", num_classes=2)
+        self.ce_loss = nn.BCEWithLogitsLoss()
+        self.dice_loss = DiceLoss()
+        self.dice_weight = dice_weight
 
     def forward(self, x):
         return self.model(x)
@@ -85,8 +109,25 @@ class LightningUNET(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         inputs, targets = batch
         logits = self.forward(inputs)
-        loss = F.binary_cross_entropy_with_logits(logits, targets)
-        self.log("Loss:", loss, on_step=True, on_epoch=True, prog_bar=True)
+
+        ce = self.ce_loss(logits, targets)
+        dice = self.dice_loss(logits, targets)
+        loss = self.dice_weight * dice + (1 - self.dice_weight) * ce
+ 
+        self.log("train_loss", loss, sync_dist=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        inputs, targets = batch
+        logits = self.forward(inputs)
+
+        ce = self.ce_loss(logits, targets)
+        dice = self.dice_loss(logits, targets)
+        loss = self.dice_weight * dice + (1 - self.dice_weight) * ce
+ 
+        self.log("val_loss", loss, sync_dist=True)
+        iou = self.iou(torch.sigmoid(logits) > 0.5, targets)
+        self.log("val_iou", iou, sync_dist=True)
         return loss
 
     def test_step(self, batch, batch_idx):
@@ -95,8 +136,7 @@ class LightningUNET(pl.LightningModule):
         preds = torch.sigmoid(logits) > 0.5
         loss = self.iou(preds, targets)
 
-        self.log("test_iou", loss, prog_bar=True)
-        
+        self.log("test_iou", loss, prog_bar=True)        
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=1e-3)
---
+++ unet/training.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=unet-training
+#SBATCH --time=72:00:00
+
+#SBATCH --nodes=2
+#SBATCH --gpus=3
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=512gb
+module load anaconda
+conda activate torch
+
+srun python3 unettraining.py
